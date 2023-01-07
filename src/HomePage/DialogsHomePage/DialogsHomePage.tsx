a5a0a6da02de9306bedc6564058a2b427cb3013e
yewhen22888 <yewhen22888@gmail.com>
2023-01-07 21:29:51 +0200

    work almost completed

--- src/HomePage/DialogsHomePage/DialogsHomePage.tsx
+++ src/HomePage/DialogsHomePage/DialogsHomePage.tsx
@@ -1,27 +1,13 @@
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"
 
-let task = [
-    { id: 1, message: 'So,are you learning react?' },
-    { id: 2, message: 'Where is my many Jony?' },
-    { id: 3, message: 'I sell snus' },
-    { id: 4, message: 'hi i rober' },
-    { id: 5, message: 'you website wery nise!' }
-
-]
 
 
-const DialogsHomePage = () => {
+
+const DialogsHomePage = (props:any) => {
     return (
-        <div>
-            {task.map((t) => {
-                return (
-                    <div>
-                        {t.message}
-                    </div>
-
-                )
-            })}
-        </div>
+      <div>
+{props.message}
+      </div>
     )
 }
 export default DialogsHomePage
--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -1,14 +1,34 @@
 import Navbar from "./NavbarHomePage/NavbarHomePage";
 import s from "./HomePage.module.css"
 import DialogsHomePage from "./DialogsHomePage/DialogsHomePage";
+import { Route, Routes } from "react-router-dom";
+
+let task = [
+    { id: '0', message: 'test' },
+    { id: '1', message: 'So,are you learning react?' },
+    { id: '2', message: 'Where is my many Jony?' },
+    { id: '3', message: 'I sell snus' },
+    { id: '4', message: 'hi i rober' },
+    { id: '5', message: ' you website wery nise!' }
+
+]
+
+let taskupgrade = task.map(el => <Route path={el.id} element={<DialogsHomePage message={el.message} />} />)
 
 const HomePage = () => {
     return (
+
         <div className={s.NavbarWraper}>
-            <div className={s.Navbar} ><Navbar /></div>
-            <div className={s.Dialogs}><DialogsHomePage   /></div>
+            <Navbar />
+            <div>
+                <Routes>
+                    {taskupgrade}
+                </Routes>
+            </div>
         </div>
-        
+
+
+
     )
 }
 
