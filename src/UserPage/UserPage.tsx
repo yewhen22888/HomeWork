86238aff744c5657c9e236828567514ecef83990
yewhen22888 <yewhen22888@gmail.com>
2023-01-16 08:56:45 +0200

    completed

--- src/UserPage/UserPage.tsx
+++ src/UserPage/UserPage.tsx
@@ -24,21 +24,36 @@ const UserPage = (props: Propstype) => {
 
   const ValueNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setNewTaskName(e.currentTarget.value)
+    setNameError(null)
+  }
+  
+  const ValueNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskNumber(e.currentTarget.value)
+    SetNumberError(null)
   }
 
   const [NewTaskNumber, setNewTaskNumber] = useState('')
+  const [NameError, setNameError] = useState<null | string>(null)
+  const [NumberError, SetNumberError] = useState<null | string>(null)
 
-  const ValueNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setNewTaskNumber(e.currentTarget.value)
-  }
+  
 
   const OnClichNameHandler = () => {
-    props.addName(NewTaskName);
+    if (NewTaskName.trim() !== "") {
+      props.addName(NewTaskName);
+    } else {
+      setNameError('Field is required')
+    }
+
     setNewTaskName('')
   }
 
   const OnClichNumberHandler = () => {
-    props.addNumber(NewTaskNumber);
+    if (NewTaskNumber.trim() !== "") {
+      props.addNumber(NewTaskNumber)
+    } else {
+      SetNumberError('Field is required')
+    }
     setNewTaskNumber('')
   }
 
@@ -55,19 +70,23 @@ const UserPage = (props: Propstype) => {
           <input disabled={props.TaskForName[0].disabled}
             placeholder='input to add User Name'
             value={NewTaskName}
-            onChange={ValueNameChange} />
+            onChange={ValueNameChange}
+            className={NameError ? s.error : ""} />
+
           <button onClick={OnClichNameHandler} disabled={props.TaskForName[0].disabled}>add</button>
         </div>
+        {NameError && <div className={s.errorMessage}>{NameError}</div>}
 
 
         <div className={s.item}>User Number"{props.TaskForNumber[0].Name}"<button onClick={props.RemoweNumber}>x</button></div>
         <div><input disabled={props.TaskForNumber[0].disabled}
           placeholder='input to add User Name'
           value={NewTaskNumber}
-          onChange={ValueNumberChange} />
+          onChange={ValueNumberChange}
+          className={NumberError ? s.error : ""} />
           <button onClick={OnClichNumberHandler} disabled={props.TaskForNumber[0].disabled}>add</button>
         </div>
-
+        {NumberError && <div className={s.errorMessage}>{NumberError}</div>}
       </ul>
     </div>
   )
